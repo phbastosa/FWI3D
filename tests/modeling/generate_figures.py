6c26b64223c169cee42d1b129579b9c07bdce02b
phbastosa <phbastosa@gmail.com>
2025-08-18 13:44:41 -0300

    modeling test concluded

--- tests/modeling/generate_figures.py
+++ tests/modeling/generate_figures.py
@@ -38,3 +38,27 @@ pyf.plot_model_3D(model, dh, slices, shots = sps_path, scale = 2.0,
                   nodes = rps_path, adjx = 0.75, dbar = 1.4, cmap = "jet",
                   cblab = "P wave velocity [km/s]")
 plt.show()
+
+ns = len(SPS)
+nr = len(RPS)
+
+data_folder = pyf.catch_parameter(parameters, "mod_output_folder")
+
+for sId in range(ns):
+
+    data_file = data_folder + f"seismogram_nt{nt}_nr{nr}_{int(dt*1e6)}us_shot_{sId+1}.bin"
+
+    seismic = pyf.read_binary_matrix(nt, nr, data_file)
+
+    scale = 5.0*np.std(seismic)
+
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    ax.set_title(f"Shot {sId+1}", fontsize = 15)
+    ax.set_ylabel("Time [s]", fontsize = 15)
+    ax.set_xlabel("Receiver index", fontsize = 15)
+
+    ax.imshow(seismic, aspect = "auto", cmap = "Greys", vmin = -scale, vmax = scale, extent = [0, nr, (nt-1)*dt, 0])
+
+    fig.tight_layout()
+    plt.show()
